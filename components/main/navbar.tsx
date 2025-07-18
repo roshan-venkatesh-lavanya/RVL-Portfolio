a2597e24a0d8c875d54b391390ecefd16c214538
roshan-venkatesh-lavanya <roshan.venla@gmail.com>
2025-07-18 23:56:33 +0530

    Update navbar.tsx

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -25,7 +25,7 @@ export const Navbar = () => {
             draggable={false}
             className="cursor-pointer"
           />
-          <div className="hidden md:flex md:selffont-bold ml-[10px] text-gray-300">ROSHAN'S PORTFOLIO</div>
+          <div className="hidden md:flex md:selffont-bold ml-[10px] text-gray-300">ROSHAN&aposS PORTFOLIO</div>
         </Link>
 
         {/* Web Navbar */}
